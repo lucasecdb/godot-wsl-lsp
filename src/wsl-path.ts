af4d6bca0b3e6af3c8e0cdac9c2a9a5fc29388f7
Lucas Cordeiro <ecdb.lucas@gmail.com>
2025-04-28 17:45:17 +0200

    Use lowercase drive name in linux conversion

--- src/wsl-path.ts
+++ src/wsl-path.ts
@@ -4,6 +4,7 @@ import * as cp from "node:child_process";
 import { getCliOption } from "./cli-flags.js";
 
 const WSLPATH_BIN = "wslpath";
+const BASE_MNT = "/mnt";
 
 const execFile = promisify(cp.execFile);
 
@@ -13,36 +14,34 @@ async function wslpath(args: string[]) {
   return stdout.trim();
 }
 
-export function convertWindowsToWslPath(filePath: string): Promise<string> {
+export async function convertWindowsToWslPath(
+  filePath: string,
+): Promise<string> {
   if (getCliOption("experimentalFastPathConversion")) {
     const parsedPath = path.win32.parse(filePath);
 
     const drive = parsedPath.root.split(":")[0];
 
-    return Promise.resolve(
-      path.posix.join(
-        BASE_MNT,
-        drive,
-        path.posix.relative(parsedPath.root, parsedPath.dir),
-        parsedPath.base,
-      ),
+    return path.posix.join(
+      BASE_MNT,
+      drive.toLowerCase(),
+      path.posix.relative(parsedPath.root, parsedPath.dir),
+      parsedPath.base,
     );
   }
 
   return wslpath([filePath]);
 }
 
-const BASE_MNT = "/mnt";
-
-export function convertWslToWindowsPath(filePath: string): Promise<string> {
+export async function convertWslToWindowsPath(
+  filePath: string,
+): Promise<string> {
   if (getCliOption("experimentalFastPathConversion")) {
     const relativeToMnt = path.relative(BASE_MNT, filePath);
 
     const [drive, ...file] = relativeToMnt.split(path.posix.sep);
 
-    return Promise.resolve(
-      `${drive}:${path.posix.sep}${file.join(path.posix.sep)}`,
-    );
+    return path.posix.join(`${drive.toUpperCase()}:`, ...file);
   }
   return wslpath(["-m", filePath]);
 }
